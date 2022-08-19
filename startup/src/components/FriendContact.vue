87970865faa8f0a551a75c6f4db415a39f10955f
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-08-18 23:03:24 -0400

    Add s7-moving-to-a-better-dev-setup

---
+++ startup/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ areDetailsVisible ? "Hide Details" : "Show Details" }}
+    </button>
+    <ul v-if="areDetailsVisible">
+      <li><strong>Name: </strong>{{ friend.name }}</li>
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      areDetailsVisible: false,
+      friend: {
+        id: "andrey",
+        name: "Andrey Lyamkin",
+        phone: "514 245 3355",
+        email: "andrey@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.areDetailsVisible = !this.areDetailsVisible;
+    },
+  },
+};
+</script>
